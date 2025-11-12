4d9a4fe9a96a87db434b7a2fe4f416a1e91f10a2
Vinicius Solon <vini.97@live.com>
2025-11-12 08:02:42 -0300

    Start!

---
+++ src/main/java/org/solon/app/AvoidingMappingStackoverflowApplication.java
@@ -0,0 +1,13 @@
+package org.solon.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvoidingMappingStackoverflowApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvoidingMappingStackoverflowApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/solon/app/Application.java
@@ -0,0 +1,13 @@
+package org.solon.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Application {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
