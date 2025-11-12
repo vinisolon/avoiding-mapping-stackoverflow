faeec5abadf61a575b58f78fcc0afbfa32981b97
Vinicius Solon <vini.97@live.com>
2025-11-12 18:34:39 -0300

    Implements stackoverflow scenario

--- src/main/java/org/solon/app/mapper/ParentMapper.java
+++ src/main/java/org/solon/app/mapper/ParentMapper.java
@@ -1,19 +1,12 @@
 package org.solon.app.mapper;
 
 import org.mapstruct.Mapper;
-import org.mapstruct.Mapping;
-import org.mapstruct.Named;
 import org.solon.app.domain.ParentDomain;
 import org.solon.app.entity.ParentEntity;
 
 @Mapper(componentModel = "spring")
 public interface ParentMapper {
 
-    @Mapping(target = "child.parents", ignore = true)
     ParentDomain from(ParentEntity entity);
-
-    @Named("mapIgnoringChild")
-    @Mapping(target = "child", ignore = true)
-    ParentDomain mapIgnoringChild(ParentEntity entity);
 
 }
